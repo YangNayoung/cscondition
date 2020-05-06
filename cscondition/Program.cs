a1fb7dd0b860d3c8856d9997ac7439a5262ae9d4
Nayoung <s2018s32@e-mirim.hs.kr>
2020-05-06 22:32:48 +0900

    #5 13-5 switch 조건문 실습

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ namespace cscondition
             // 홀짝 구하기
             Console.WriteLine("숫자 입력: ");
             int input = int.Parse(Console.ReadLine());
-
+            /* 
             if (input % 2 == 0)
             {
                 Console.WriteLine("짝수입니다");
@@ -21,6 +22,16 @@ namespace cscondition
             else
             {
                 Console.WriteLine("홀수입니다");
+            }
+            */
+            switch (input % 2)
+            {
+                case 0:
+                    Console.WriteLine("짝수입니다");
+                    break;
+                case 1:
+                    Console.WriteLine("홀수입니다");
+                    break;
             }
 
             // 현재 시간 구하기
@@ -36,13 +47,39 @@ namespace cscondition
             {
                 Console.WriteLine("아침 먹을 시간입니다");
             } else  if (DateTime.Now.Hour < 15)
-                {
+            {
                     Console.WriteLine("점심 먹을 시간입니다");
-                }
-                else
-                { 
-                    Console.WriteLine("저녁 먹을 시간입니다");
-                }            
+            }
+            else
+            {
+                Console.WriteLine("저녁 먹을 시간입니다");
+            }
+
+            Console.Write("이번 달은 몇 월인가요: ");
+            int input2 = int.Parse(Console.ReadLine());
+            switch (input2)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울입니다");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄입니다");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름입니다");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을입니다");
+                    break;
+            }
 
         }
     }
